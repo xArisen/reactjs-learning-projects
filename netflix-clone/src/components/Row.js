f9b23980eed3de6fbac3fdc742c8a8e9115b670a
xArisen <czapiewskipiotr04@gmail.com>
2021-01-28 12:42:24 +0100

    Added: movie rows, banner at top

--- netflix-clone/src/components/Row.js
+++ netflix-clone/src/components/Row.js
@@ -1,27 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import axios from '../axios';
+import '../style/Row.css';
 
-const Row = ({title, fetchUrl}) =>{
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+const Row = ({title, fetchUrl, isLargeRow}) =>{
     const [movies, setMovies] = useState([]);
 
     // A snippet of code which runs based on a specific condition
     useEffect(() => {
-        async function fecthData(){
+        async function fetchData(){
             const request = await axios.get(fetchUrl);
             setMovies(request.data.results);
             return request;
         }
-        fecthData();
+        fetchData();
     }, [fetchUrl]); //if [] blank we tell it to run once at start and never again, if [movies] then it runs every time movies changes
 
     return (
-        <div>
-            <h2>    
-                {title}
-                
-            </h2>
+        <div className="row">
+            <h2>{title}</h2>
+            <div className="row_posters">
+            {/* several row_posters */}
 
-            {/* containter -> posters */}
+            {movies.map((movie) => {
+                return <img key={movie.id} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} className={`row_poster ${isLargeRow && "row_posterLarge"}`}/>
+            })}
+
+            </div>
         </div>
     )
 }
